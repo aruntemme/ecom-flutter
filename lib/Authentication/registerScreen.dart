032a334b43b10f2c1f0e066ce6d16544e2c181d6
aruntemme <arungauthamk@gmail.com>
2020-10-07 21:18:36 +0530

    registerScreen UI

--- lib/Authentication/registerScreen.dart
+++ lib/Authentication/registerScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -6,8 +8,52 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _nameTextEditingController =
+      TextEditingController();
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+  final TextEditingController _cPasswordTextEditingController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String userImageurl = "";
+  File _imageFile;
+
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    double _screenWidth = MediaQuery.of(context).size.width,
+        _screenHeight = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+        child: Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          InkWell(
+            onTap: () => print("selected"),
+            child: CircleAvatar(
+              radius: _screenWidth * 0.15,
+              backgroundColor: Colors.white,
+              backgroundImage:
+                  _imageFile == null ? null : FileImage(_imageFile),
+              child: _imageFile == null
+                  ? Icon(
+                      Icons.add_a_photo_outlined,
+                      size: _screenWidth * 0.15,
+                      color: Colors.grey,
+                    )
+                  : null,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
